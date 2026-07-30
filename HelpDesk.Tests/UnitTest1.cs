89b1e9c5b8799601dfa1a394af48e22952fd8237
Benjamin Martinez <santiipettinati2@gmail.com>
2026-07-30 19:43:15 -0300

    Primer clase

---
+++ HelpDesk.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace HelpDesk.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
